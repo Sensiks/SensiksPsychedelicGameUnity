453e847faa74e28d9d06f05fe5d9db082f21c073
DESKTOP-VFJ76H8\caspe <casperarends1910@gmail.com>
2023-04-24 17:12:55 +0200

    code for pressure minigame

--- Assets/pressureMinigame.cs
+++ Assets/pressureMinigame.cs
@@ -10,5 +10,18 @@ public class PressureMinigame : MonoBehaviour
     [SerializeField]
     private Transform cursur;
 
-    private float cursurPosition, cursurGoal;
+    private float cursurPosition, cursurGoal, cursurTimer;
+    [SerializeField]
+    private float timerMultiplier = 3f;
+
+    public void Update()
+    {
+        cursurTimer -= Time.deltaTime;
+        if (cursurTimer <= 0f)
+        {
+            cursurTimer = UnityEngine.Random.value * timerMultiplier;
+        }
+    }
+
+        
 }
