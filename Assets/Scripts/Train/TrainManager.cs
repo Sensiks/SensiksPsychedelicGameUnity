b9ee311830f2a2fee9b953b6a9dfe4519bd24dcd
DESKTOP-VFJ76H8\caspe <casperarends1910@gmail.com>
2023-06-05 13:33:19 +0200

    train acceleration

--- Assets/Scripts/Train/TrainManager.cs
+++ Assets/Scripts/Train/TrainManager.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class TrainManager : MonoBehaviour
 {
     [SerializeField] private CinemachineDollyCart train;
+    [SerializeField] private CinemachineDollyCart coalCart;
+    [SerializeField] private CinemachineDollyCart coupe;
     [SerializeField] private AudioSource trainAudioSource;
     [SerializeField] private AudioClip trainBackgroundSound;
+
+    private float acceleration = 1f;
+    private float currentSpeed = 0f;
     
 
     private void Start()
@@ -16,20 +21,37 @@ public class TrainManager : MonoBehaviour
         
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         
     }
 
     public void TrainSpeedChanger(float newSpeed)
     {
-        train.m_Speed = newSpeed;
+        StartCoroutine(IncreaseSpeed(newSpeed));
+
+        
 
         if(train.m_Speed > 0f)
         {
             trainAudioSource.clip = trainBackgroundSound;
             trainAudioSource.Play();
             trainAudioSource.loop = true;
+        }
+    }
+
+    private IEnumerator IncreaseSpeed(float newSpeed)
+    {
+        while (currentSpeed < newSpeed)
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+            //currentSpeed = Mathf.Min(currentSpeed, newSpeed);
+
+            train.m_Speed = currentSpeed;
+            coalCart.m_Speed = currentSpeed;
+            coupe.m_Speed = currentSpeed;
+
+            yield return null;
         }
     }
 }
